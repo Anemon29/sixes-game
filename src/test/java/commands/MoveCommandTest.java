56d42147951134de8a0148df4d6792226172d8d4
Anemon29 <sz.janikowski29@gmail.com>
2018-11-12 21:15:24 +0100

    Initial Commit

---
+++ src/test/java/commands/MoveCommandTest.java
@@ -0,0 +1,12 @@
+package commands;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MoveCommandTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
---
+++ src/test/java/engine/MoveCheckerTest.java
@@ -0,0 +1,12 @@
+package engine;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MoveCheckerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
