bc258d2217caee23c7fa132c4c69fa5e4b55c206
Szymon Janikowski <sz.janikowski29@gmail.com>
2018-11-12 22:05:46 +0100

    Add few more specific classes to model

---
+++ src/main/java/model/CardSuit.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum CardSuit {
+    CLUBS, DIAMONDS, HEARTS, SPADES
+}
---
+++ src/test/java/model/DeckTest.java
@@ -0,0 +1,12 @@
+package model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class DeckTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
