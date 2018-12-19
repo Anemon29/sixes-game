7f118eb5c9c0c868f2ff788e1d66d288510747b2
Szymon Janikowski <sz.janikowski29@gmail.com>
2018-12-19 03:25:19 +0100

    Add feature of removing low cards from board

--- src/main/java/pl/edu/agh/sixes/engine/validation/chain/DefaultValidationChain.java
+++ src/main/java/pl/edu/agh/sixes/engine/validation/chain/DefaultValidationChain.java
@@ -1,4 +1,16 @@
 package pl.edu.agh.sixes.engine.validation.chain;
 
+import pl.edu.agh.sixes.model.CardContainer;
+
 public class DefaultValidationChain {
+
+    public boolean validate(CardContainer first, CardContainer second){
+        if (first.getContent().get().equals(second.getContent().get())) {
+            if (first.getCoordinates().get().equals(second.getCoordinates().get())){
+                throw new IllegalArgumentException("You can't choose the same card twice");
+            }
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/pl/edu/agh/sixes/model/Coordinates.java
+++ src/main/java/pl/edu/agh/sixes/model/Coordinates.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.sixes.model;
 
+import java.util.Objects;
+
 public class Coordinates {
 
     private final int rowId;
@@ -22,6 +24,20 @@ public class Coordinates {
         return String.format("(%d, %d)", rowId, columnId);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return rowId == that.rowId &&
+                columnId == that.columnId;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(rowId, columnId);
+    }
 
     public int getRowId() {
         return rowId;
