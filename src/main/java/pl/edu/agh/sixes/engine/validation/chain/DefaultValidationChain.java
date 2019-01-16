5a32f1933f0ce50c334922f4c4acb68884407c71
Jean9797 <rodzonjan97@gmail.com>
2019-01-16 13:35:54 +0100

    Same cards bug fixed.

--- src/main/java/pl/edu/agh/sixes/engine/validation/chain/DefaultValidationChain.java
+++ src/main/java/pl/edu/agh/sixes/engine/validation/chain/DefaultValidationChain.java
@@ -6,7 +6,7 @@ public class DefaultValidationChain implements ValidationChain{
 
     public boolean validate(CardContainer first, CardContainer second){
         if (first.getContent().equals(second.getContent())) {
-            if (first.getCoordinates().equals(second.getCoordinates())){
+            if (first.getCoordinates().equals(second.getCoordinates()) && first.getPlace().equals(second.getPlace())){
                 throw new IllegalArgumentException("You can't choose the same card twice");
             }
             return true;
